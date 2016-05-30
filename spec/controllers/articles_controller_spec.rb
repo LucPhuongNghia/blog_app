c94819bf686e2bb70035a9b02de661aa1d0fbacf
nghia <nghialp@tocdep.vn>
2016-05-30 10:57:43 +0000

    Restrict access in controller action

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,8 +1,36 @@
 require 'rails_helper'
+require 'support/macros'
 
 RSpec.describe ArticlesController, type: :controller do
 
-  describe "GET #index" do
+  describe "GET #edit" do
+    before do
+      @john = User.create(email: 'john@example.com', password: 'password')
+    end
+    
+    context "Owner is allowed to edit his article" do
+      it "render the edit template" do
+        login_user @john
+        article = Article.create(title: 'first article', body: 'Body of first article', user: @john)
+        get :edit, id: article
+        
+        expect(response).to render_template :edit
+      end
+    end
+    
+    context "none owner are not allowed to edit other users articles" do
+      it "redirect to the root path" do
+        jo = User.create(email: 'jo@example.com', password: 'password')
+        login_user jo
+        article = Article.create(title: 'first article', body: 'Body of first article', user: @john)
+        get :edit, id: article
+        
+        expect(response).to redirect_to(root_path)
+        message = "You can only edit your own articles"
+        expect(flash[:danger]).to eq(message) 
+      end
+    end
+    
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
